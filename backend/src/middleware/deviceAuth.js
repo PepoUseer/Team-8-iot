9a9444fbafa1a14ac7942abcab7a28ccfe559a6f
Daniel46464 <117084549+Daniel46464@users.noreply.github.com>
2026-04-27 13:20:36 +0200

    Remove log

--- backend/src/middleware/deviceAuth.js
+++ backend/src/middleware/deviceAuth.js
@@ -1,6 +1,5 @@
 const validateApiKey = (req, res, next) => {
     const clientApiKey = req.header("device-api-key");
-    console.log("KEY", clientApiKey, process.env.DEVICE_API_KEY);
     if (!clientApiKey || clientApiKey !== process.env.DEVICE_API_KEY) {
         return res.status(401).json({ 
             error: "Unauthorized: Invalid or missing API key."
