8335776a88a7fb18e955dd676356bb377faff3bd
Daniel46464 <117084549+Daniel46464@users.noreply.github.com>
2026-04-30 13:08:37 +0200

    Add user auth to routes

--- backend/src/routes/devices.js
+++ backend/src/routes/devices.js
@@ -1,18 +1,19 @@
 import express from "express";
 import validateApiKey from "../middleware/deviceAuth.js";
+import verifyAuth from "../middleware/userAuth.js";
 const router = express.Router();
 
 import DeviceController from "../controllers/deviceController.js";
 const dc = new DeviceController();
 
 router.get("/");
-router.get("/:id", dc.get.bind(dc));
-router.post("/", dc.create.bind(dc));
+router.get("/:id", verifyAuth, dc.get.bind(dc));
+router.post("/", verifyAuth, dc.create.bind(dc));
 router.post("/alias", validateApiKey, dc.createOrGet.bind(dc));
-router.patch("/:id", dc.update.bind(dc));
-router.get("/:id/sensors", dc.getSensors.bind(dc));
+router.patch("/:id", verifyAuth, dc.update.bind(dc));
+router.get("/:id/sensors", verifyAuth, dc.getSensors.bind(dc));
 router.post("/add");
 router.get("/:id/latest");
-router.delete("/:id", dc.delete.bind(dc));
+router.delete("/:id", verifyAuth, dc.delete.bind(dc));
 
 export default router;
